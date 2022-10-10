4966a78acb90f83d5c87050e09ccd25bd58fbdd8
Lucas Santos <lucas.silva1203@outlook.com>
2022-10-10 19:43:54 -0300

    First version

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
+let screen = document.querySelector("#screen")
 let digits = document.querySelectorAll(".digit")
 digits.forEach((digit) => {
-    digit.addEventListener("click", getFirstValue)
+    digit.addEventListener("click", displayValue)
 })
+
+let arr = []
+let clearButton = document.querySelector("#Clear")
+clearButton.addEventListener("click", () => {
+    screen.textContent = ""
+    arr = []
+})
+
+
+let operator
+const add = (a, b) => a + b
+let addButton = document.querySelector("#add")
+addButton.addEventListener("click", pushValue)
+
+const divide = (a, b) => a + b
+let divideButton = document.querySelector("#divide")
+divideButton.addEventListener("click", pushValue)
+
+const multiply = (a, b) => a + b
+let multiplyButton = document.querySelector("#multiply")
+multiplyButton.addEventListener("click", pushValue)
+
+const substract = (a, b) => a + b
+let substractButton = document.querySelector("#substract")
+substractButton.addEventListener("click", pushValue)
+
+
+
+let equals = document.querySelector("#equals")
+equals.addEventListener("click", () => {
+    pushValue()
+    calc(arr)
+})
+function calc(array){
+    let total = 0
+    total = array.reduce(operator)
+    return total
+}
+
+function pushValue(){
+    if(screen.textContent !== ""){
+        console.log(screen.textContent)
+        arr.push(parseInt(screen.textContent))
+    }
+    if(arr.length == 2){
+        calc(arr)
+        arr = [calc(arr)]
+        console.log(`calculado: ${calc(arr)}`, arr)
+    }
+    screen.textContent = ""
+}
+
+
+function displayValue(){
+    screen.textContent += this.value
+}
 
