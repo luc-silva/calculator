f1c8e44b95b599e0a76aa95ff7437ea66b2c17b9
Lucas Santos <lucas.silva1203@outlook.com>
2022-10-12 20:30:37 -0300

    Add clear and backspace functions

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let calcArray = []
 let operation = null
 
 const add = (a, b) => a + b
+const substract = (a, b) => a - b
 
 let addButton = document.querySelector("#add")
 addButton.addEventListener("click", () => {
@@ -37,7 +38,20 @@ function aoi(){
     }
 }
 
-let clearButton = document.querySelector
+let clearButton = document.querySelector("#clear")
+clearButton.addEventListener("click", () => {
+    mainScreen.textContent = ""
+    calculusScreen.textContent = ""
+    operation = null
+})
+
+let backspaceButton = document.querySelector("#backspace")
+backspaceButton.addEventListener("click", () => {
+    let splited = mainScreen.textContent.trim().split("")
+    splited.pop()
+    mainScreen.textContent = splited.join("")
+})
+
 
 function checker(){
     if(calcArray.length == 2){
