b11bad6f660b1b129d81ec6195e376ff479cd28e
Lucas Santos <lucas.silva1203@outlook.com>
2022-10-12 17:17:03 -0300

    Modify the whole script
    Remove displayValue(), Add calculusScreen variable and logic
    Modify checker() and operations

--- script.js
+++ script.js
@@ -1,62 +1,50 @@
 let screen = document.querySelector("#screen")
+let calculusScreen = document.querySelector("#calculus")
+
+let operator
+
+let firstValue = ""
 let digits = document.querySelectorAll(".digit")
 digits.forEach((digit) => {
-    digit.addEventListener("click", displayValue)
+    digit.addEventListener("click", function (){
+        screen.textContent += this.value
+        firstValue += this.value
+    })
 })
 
 let arr = []
-let clearButton = document.querySelector("#Clear")
-clearButton.addEventListener("click", () => {
-    screen.textContent = ""
-    arr = []
-})
-
-
-let operator
 const add = (a, b) => a + b
 let addButton = document.querySelector("#add")
-addButton.addEventListener("click", pushValue)
-
-const divide = (a, b) => a + b
-let divideButton = document.querySelector("#divide")
-divideButton.addEventListener("click", pushValue)
-
-const multiply = (a, b) => a + b
-let multiplyButton = document.querySelector("#multiply")
-multiplyButton.addEventListener("click", pushValue)
-
-const substract = (a, b) => a + b
-let substractButton = document.querySelector("#substract")
-substractButton.addEventListener("click", pushValue)
-
-
+addButton.addEventListener("click", () => {
+    operator = add
+    screen.textContent = ""
+    if(firstValue){
+        calculusScreen.textContent = `${firstValue} +`
+        arr.push(parseFloat(firstValue))
+    }
+    firstValue = ""
+})
 
 let equals = document.querySelector("#equals")
 equals.addEventListener("click", () => {
-    pushValue()
-    calc(arr)
+    if(firstValue){
+        calculusScreen.textContent += ` ${firstValue}`
+        arr.push(parseFloat(firstValue))
+        firstValue = ""
+        checker()  
+    }
 })
+function checker(){
+    if(arr.length == 2){
+        calc(arr)
+        // console.log(`calculado:`, arr)
+        screen.textContent = arr[0]
+    }
+    // screen.textContent = ""
+}
 function calc(array){
     let total = 0
     total = array.reduce(operator)
-    return total
-}
-
-function pushValue(){
-    if(screen.textContent !== ""){
-        console.log(screen.textContent)
-        arr.push(parseInt(screen.textContent))
-    }
-    if(arr.length == 2){
-        calc(arr)
-        arr = [calc(arr)]
-        console.log(`calculado: ${calc(arr)}`, arr)
-    }
-    screen.textContent = ""
-}
-
-
-function displayValue(){
-    screen.textContent += this.value
+    arr = [total]
 }
 
